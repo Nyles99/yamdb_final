03fc26f147ffc6f94c9e00cfbc8fc060d43c4e77
Никита Молчанов <nylesszpg@gmail.com>
2023-05-31 10:05:31 +0300

    start v19

--- api_yamdb/reviews/views.py
+++ api_yamdb/reviews/views.py
@@ -15,10 +15,10 @@ class ReviewViewSet(viewsets.ModelViewSet):
 
     def get_title(self):
         return get_object_or_404(Title, id=self.kwargs.get('title_id'))
-        
+
     def get_queryset(self):
         title_id = self.kwargs.get("title_id")
-        return Review.objects.filter(title=title_id)        
+        return Review.objects.filter(title=title_id)
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user, title=self.get_title())
@@ -32,11 +32,11 @@ class CommentViewSet(viewsets.ModelViewSet):
     )
 
     def get_review(self):
-         return get_object_or_404(Review, id=self.kwargs.get('review_id'))
-        
+        return get_object_or_404(Review, id=self.kwargs.get('review_id'))
+
     def get_queryset(self):
         review_id = self.kwargs.get("review_id")
         return Comments.objects.filter(review=review_id)
-        
+
     def perform_create(self, serializer):
         serializer.save(author=self.request.user, review=self.get_review())
